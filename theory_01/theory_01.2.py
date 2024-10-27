f8fcbbe1c7a13e9b95833a50b6f43eb62225dc38
pstrimtu <pstrimtu@techfak.de>
2024-10-27 18:28:48 +0100

    small changes on theory_01.2.py

--- theory_01/theory_01.2.py
+++ theory_01/theory_01.2.py
@@ -4,23 +4,25 @@ x_1 = np.array([1, 0.5])
 x_2 = np.array([-0.5, -1])
 x_3 = np.array([-0.5, 0])
 
-y = np.array([1, 0, 0])
+y_all = np.array([1, 0, 0])
 
 w_1 = (1 / np.sqrt(2)) * np.array([1, 1])
 w_2 = np.array([1, 0])
 
+
 def sgd(u):
     return 1 / (1 + np.exp(-u))
 
-def neg_nll(w, X, y):
-    p = sgd(np.dot(w, X.T)) #transponiert
-    nll = -(np.sum(y * np.log(p) + (1 - y) * np.log(1 - p)))
 
+def neg_nll(w, x, y):
+    p = sgd(np.dot(w, x.T))  # transponiert
+    nll = -(np.sum(y * np.log(p) + (1 - y) * np.log(1 - p)))
     return nll
+
 
 X = np.array([x_1, x_2, x_3])
 
-nll_w1 = neg_nll(w_1, X, y)
-nll_w2 = neg_nll(w_2, X, y)
+nll_w1 = neg_nll(w_1, X, y_all)
+nll_w2 = neg_nll(w_2, X, y_all)
 
 print(nll_w1, nll_w2)
